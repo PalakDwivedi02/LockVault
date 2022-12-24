cad2449648a10d08c178691a64036e78309b0f60
PalakDwivedi02 <98268910+PalakDwivedi02@users.noreply.github.com>
2022-12-24 17:00:26 +0530

    Add files via upload

---
+++ pass_gen.py
@@ -0,0 +1,24 @@
+import random
+import string
+
+alpha_lower="abcdefghijklmnopqrstuvwxyz"
+
+alpha_upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+numbers="1234567890"
+
+symbols="!@#$%^&*-_"
+
+length=16
+
+str=alpha_lower+numbers+alpha_upper+symbols
+
+ch="".join(random.sample(str,length))
+
+print(ch)
+f=open("Names.txt","a")
+f.write("\n"+ch)
+#f.truncate(0)
+f.close()
+f=open("Names.txt","r")
+print(f.read())
